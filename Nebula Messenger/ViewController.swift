7acd10c2aa1ef92c94c7dd561802343adcfa2fc0
Shelby McCowan <shelbymccowan@shelbys-macbook-air.local>
2018-09-19 00:21:56 -0600

    Initial Commit

---
+++ Nebula Messenger/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Nebula Messenger
+//
+//  Created by Shelby McCowan on 9/19/18.
+//  Copyright © 2018 Shelby McCowan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Nebula MessengerTests/Nebula_MessengerTests.swift
@@ -0,0 +1,34 @@
+//
+//  Nebula_MessengerTests.swift
+//  Nebula MessengerTests
+//
+//  Created by Shelby McCowan on 9/19/18.
+//  Copyright © 2018 Shelby McCowan. All rights reserved.
+//
+
+import XCTest
+@testable import Nebula_Messenger
+
+class Nebula_MessengerTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
