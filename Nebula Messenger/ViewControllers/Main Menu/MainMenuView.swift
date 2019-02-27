e06d1b7fc3e4cc3a954def2c16ef0a52f66a7e2e
Shelby McCowan <smccowan33@gmail.com>
2019-02-27 01:52:11 -0700

    Getting ready to separate out Main Menu VC into VC and a UIView. Not doing it tonight though, I'm tired.

---
+++ Nebula Messenger/ViewControllers/Main Menu/MainMenuView.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuView.swift
+//  Nebula Messenger
+//
+//  Created by Shelby McCowan on 2/27/19.
+//  Copyright © 2019 Shelby McCowan. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuView: UIView {
+
+}
