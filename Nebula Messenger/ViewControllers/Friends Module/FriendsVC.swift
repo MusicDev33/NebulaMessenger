8dcb9b4d64e7d84a8cd6505acd012525e706b260
Shelby McCowan <smccowan33@gmail.com>
2019-02-27 01:56:23 -0700

    Reorg of FriendsVC

--- Nebula Messenger/ViewControllers/Friends Module/FriendsVC.swift
+++ Nebula Messenger/ViewControllers/Friends Module/FriendsVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, UIGestureRecognizerDelegate, CAAnimationDelegate, UINavigationControllerDelegate {
+class FriendsVC: UIViewController, UIGestureRecognizerDelegate, CAAnimationDelegate {
     
     // Navbar goodies
     // These are here until I find a better way to do this
@@ -29,9 +29,163 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
     var currentRequestedFriends = [String]()
     
     var addFriendButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.white
+        navigationItem.hidesBackButton = true
+        navigationController?.delegate = self
+        self.searchBar.delegate = self
+        self.searchBar.isUserInteractionEnabled = false
+        
+        for subview in searchBar.subviews  {
+            for subSubview in subview.subviews  {
+                if let textField = subSubview as? UITextField {
+                    UIView.animate(withDuration: 0.4, animations: {
+                        textField.backgroundColor = panelColorOneAlt
+                    })
+                }
+            }
+        }
+        
+        topView = FriendsView(frame: self.view.frame)
+        self.view.addSubview(topView)
+        friendTable = topView.friendsTable
+        friendTable.delegate = self
+        friendTable.dataSource = self
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipedLeft))
+        swipeLeft.direction = .left
+        swipeLeft.delegate = self
+        
+        backButton = UIButton(type: .system)
+        backButton.setImage(UIImage(named: "ForwardArrowBlack"), for: .normal)
+        backButton.frame = CGRect(x: self.view.frame.width, y: self.view.frame.height, width: 0, height: 0)
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        backButton.addTarget(self, action: #selector(swipedLeft), for: .touchUpInside)
+        navigationController?.navigationBar.addSubview(backButton)
+        
+        backButton.rightAnchor.constraint(equalTo: (navigationController?.navigationBar.rightAnchor)!, constant: 30).isActive = true
+        backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        self.navigationController?.navigationBar.layoutIfNeeded()
+        
+        currentRequestedFriends = GlobalUser.requestedFriends
+        
+        topView.addGestureRecognizer(swipeLeft)
+        
+        UIView.transition(with: self.addFriendButton,
+                          duration: 0.2,
+                          options: .transitionFlipFromLeft,
+                          animations: {
+                            self.addFriendButton.setImage(UIImage(named: "AddFriendBlack"), for: .normal)})
+        
+        //self.addFriendButton.setImage(UIImage(named: "AddFriendBlack"), for: .normal)
+        self.addFriendButton.removeTarget(nil, action: nil, for: .allEvents)
+        self.addFriendButton.addTarget(self, action: #selector(addFriendButtonPressed), for: .touchUpInside)
+    }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        navigationController?.isNavigationBarHidden = false
+        
+        backButton.rightAnchor.constraint(equalTo: (navigationController?.navigationBar.rightAnchor)!, constant: -6).isActive = true
+        backButton.centerYAnchor.constraint(equalTo: (navigationController?.navigationBar.centerYAnchor)!).isActive = true
+        
+        exitSearchRightAnchor?.constant -= 30
+        searchBarRightAnchor?.constant -= 30
+        
+        UIView.animate(withDuration: 0.2, animations: {
+            self.navigationController?.navigationBar.layoutIfNeeded()
+        })
+    }
+}
+
+
+// MARK: UISearchBar Ext.
+extension FriendsVC: UISearchResultsUpdating, UISearchBarDelegate {
     
-    // Tableview methods
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        self.searchMode = true
+        self.friendTable.contentInset = UIEdgeInsets(top: -35, left: 0, bottom: 0, right: 0)
+        self.searchResults = GlobalUser.requestedFriends
+        self.friendTable.reloadData()
+        searchBarRightAnchor?.constant -= 40
+        UIView.animate(withDuration: 0.2, animations: {
+            self.view.layoutIfNeeded()
+            self.navigationController?.navigationBar.layoutIfNeeded()
+        })
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        if searchBar.text == ""{
+            self.searchMode = false
+        }
+        self.friendTable.reloadData()
+        
+        searchBarRightAnchor?.constant = -38
+        UIView.animate(withDuration: 0.2, animations: {
+            self.view.layoutIfNeeded()
+            self.navigationController?.navigationBar.layoutIfNeeded()
+        })
+        
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        self.searchMode = false
+        self.friendTable.contentInset = UIEdgeInsets(top: -35, left: 0, bottom: 0, right: 0)
+        self.friendTable.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            self.searchResults = GlobalUser.requestedFriends
+            self.friendTable.reloadData()
+        }else if searchText.count > 0{
+            if self.searchMode{
+                self.searchResults = [String]()
+                for i in GlobalUser.requestedFriends{
+                    if i.lowercased().hasPrefix(searchText.lowercased()){
+                        self.searchResults.append(i)
+                    }
+                }
+                self.friendTable.reloadData()
+            }
+        }else{
+            if self.searchMode{
+                self.searchResults = GlobalUser.requestedFriends
+            }else{
+                self.searchResults = []
+            }
+            self.friendTable.reloadData()
+        }
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+        
+        if searchString == "" {
+            self.friendTable.reloadData()
+        }else if searchString!.count > 0{
+            FriendRoutes.searchFriends(characters: searchString!){friends in
+                self.searchResults = friends
+            }
+            self.friendTable.reloadData()
+        }else{
+            self.searchResults = []
+            self.friendTable.reloadData()
+        }
+    }
+}
+
+
+// MARK: UITableView Ext.
+extension FriendsVC: UITableViewDelegate, UITableViewDataSource{
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == 0{
             if searchMode{
@@ -109,13 +263,12 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 20.0
     }
+}
+
+
+// MARK: UINavigationControllerDelegate Ext.
+extension FriendsVC: UINavigationControllerDelegate{
     
-    // Set up tableview
-    func setupTableview(){
-        
-    }
-    
-    // Actions
     func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
         
         switch operation {
@@ -125,7 +278,11 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
             return CustomAnim(duration: TimeInterval(UINavigationController.hideShowBarDuration), isPresenting: false, direction: .toLeft)
         }
     }
+}
 
+
+// MARK: Listener Ext.
+extension FriendsVC{
     
     @objc func swipedLeft(){
         self.backButton.removeFromSuperview()
@@ -144,154 +301,5 @@ class FriendsVC: UIViewController, UITableViewDelegate, UITableViewDataSource, U
         let addVC = AddFriendVC()
         addVC.modalPresentationStyle = .overCurrentContext
         self.present(addVC, animated: true, completion: nil)
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.view.backgroundColor = UIColor.white
-        navigationItem.hidesBackButton = true
-        navigationController?.delegate = self
-        self.searchBar.delegate = self
-        self.searchBar.isUserInteractionEnabled = false
-        
-        for subview in searchBar.subviews  {
-            for subSubview in subview.subviews  {
-                if let textField = subSubview as? UITextField {
-                    UIView.animate(withDuration: 0.4, animations: {
-                        textField.backgroundColor = panelColorOneAlt
-                    })
-                }
-            }
-        }
-        
-        topView = FriendsView(frame: self.view.frame)
-        self.view.addSubview(topView)
-        friendTable = topView.friendsTable
-        friendTable.delegate = self
-        friendTable.dataSource = self
-        
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(swipedLeft))
-        swipeLeft.direction = .left
-        swipeLeft.delegate = self
-        
-        backButton = UIButton(type: .system)
-        backButton.setImage(UIImage(named: "ForwardArrowBlack"), for: .normal)
-        backButton.frame = CGRect(x: self.view.frame.width, y: self.view.frame.height, width: 0, height: 0)
-        backButton.translatesAutoresizingMaskIntoConstraints = false
-        backButton.addTarget(self, action: #selector(swipedLeft), for: .touchUpInside)
-        navigationController?.navigationBar.addSubview(backButton)
-        
-        backButton.rightAnchor.constraint(equalTo: (navigationController?.navigationBar.rightAnchor)!, constant: 30).isActive = true
-        backButton.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        backButton.heightAnchor.constraint(equalToConstant: 30).isActive = true
-        self.navigationController?.navigationBar.layoutIfNeeded()
-        
-        currentRequestedFriends = GlobalUser.requestedFriends
-        
-        topView.addGestureRecognizer(swipeLeft)
-        
-        UIView.transition(with: self.addFriendButton,
-                          duration: 0.2,
-                          options: .transitionFlipFromLeft,
-                          animations: {
-                            self.addFriendButton.setImage(UIImage(named: "AddFriendBlack"), for: .normal)})
-        
-        //self.addFriendButton.setImage(UIImage(named: "AddFriendBlack"), for: .normal)
-        self.addFriendButton.removeTarget(nil, action: nil, for: .allEvents)
-        self.addFriendButton.addTarget(self, action: #selector(addFriendButtonPressed), for: .touchUpInside)
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        navigationController?.isNavigationBarHidden = false
-        
-        backButton.rightAnchor.constraint(equalTo: (navigationController?.navigationBar.rightAnchor)!, constant: -6).isActive = true
-        backButton.centerYAnchor.constraint(equalTo: (navigationController?.navigationBar.centerYAnchor)!).isActive = true
-        
-        exitSearchRightAnchor?.constant -= 30
-        searchBarRightAnchor?.constant -= 30
-        
-        UIView.animate(withDuration: 0.2, animations: {
-            self.navigationController?.navigationBar.layoutIfNeeded()
-        })
-    }
-}
-
-extension FriendsVC: UISearchResultsUpdating, UISearchBarDelegate {
-    
-    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
-        self.searchMode = true
-        self.friendTable.contentInset = UIEdgeInsets(top: -35, left: 0, bottom: 0, right: 0)
-        self.searchResults = GlobalUser.requestedFriends
-        self.friendTable.reloadData()
-        searchBarRightAnchor?.constant -= 40
-        UIView.animate(withDuration: 0.2, animations: {
-            self.view.layoutIfNeeded()
-            self.navigationController?.navigationBar.layoutIfNeeded()
-        })
-    }
-    
-    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        if searchBar.text == ""{
-            self.searchMode = false
-        }
-        self.friendTable.reloadData()
-        
-        searchBarRightAnchor?.constant = -38
-        UIView.animate(withDuration: 0.2, animations: {
-            self.view.layoutIfNeeded()
-            self.navigationController?.navigationBar.layoutIfNeeded()
-        })
-        
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        self.searchMode = false
-        self.friendTable.contentInset = UIEdgeInsets(top: -35, left: 0, bottom: 0, right: 0)
-        self.friendTable.reloadData()
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if searchText == "" {
-            self.searchResults = GlobalUser.requestedFriends
-            self.friendTable.reloadData()
-        }else if searchText.count > 0{
-            if self.searchMode{
-                self.searchResults = [String]()
-                for i in GlobalUser.requestedFriends{
-                    if i.lowercased().hasPrefix(searchText.lowercased()){
-                        self.searchResults.append(i)
-                    }
-                }
-                self.friendTable.reloadData()
-            }
-        }else{
-            if self.searchMode{
-                self.searchResults = GlobalUser.requestedFriends
-            }else{
-                self.searchResults = []
-            }
-            self.friendTable.reloadData()
-        }
-    }
-    
-    func updateSearchResults(for searchController: UISearchController) {
-        let searchString = searchController.searchBar.text
-        
-        if searchString == "" {
-            self.friendTable.reloadData()
-        }else if searchString!.count > 0{
-            FriendRoutes.searchFriends(characters: searchString!){friends in
-                self.searchResults = friends
-            }
-            self.friendTable.reloadData()
-        }else{
-            self.searchResults = []
-            self.friendTable.reloadData()
-        }
     }
 }
