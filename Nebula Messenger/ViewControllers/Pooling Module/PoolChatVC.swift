9075aa469741cb70f501322e14a88a9e69b51791
Shelby McCowan <smccowan33@gmail.com>
2019-03-18 00:14:51 -0600

    Reorganized PoolChatVC.

--- Nebula Messenger/ViewControllers/Pooling Module/PoolChatVC.swift
+++ Nebula Messenger/ViewControllers/Pooling Module/PoolChatVC.swift
@@ -10,7 +10,7 @@ import UIKit
 import Alamofire
 import FirebaseMessaging
 
-class PoolChatVC: MessengerBaseVC, UICollectionViewDelegate, UICollectionViewDataSource, UITextViewDelegate, UICollectionViewDelegateFlowLayout, UIGestureRecognizerDelegate {
+class PoolChatVC: MessengerBaseVC {
     
     var currentPoolMessages = [TerseMessage]()
     var poolId = ""
@@ -20,63 +20,7 @@ class PoolChatVC: MessengerBaseVC, UICollectionViewDelegate, UICollectionViewDat
     
     var messagesCollectionBottomConstraint: NSLayoutConstraint?
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return currentPoolMessages.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "messageBubble", for: indexPath) as! MessageBubble
-        let text = self.currentPoolMessages[indexPath.row].body
-        
-        cell.textView.text = text
-        cell.bubbleWidthAnchor?.constant = findSize(text: text!, label: cell.textView).width + 32
-        cell.senderLabel.text = ""
-        
-        cell.textView.textColor = UIColor.white
-        
-        cell.senderHeightAnchor?.constant = 0
-        
-        if self.currentPoolMessages[indexPath.row].sender == GlobalUser.username{
-            cell.senderHeightAnchor?.constant = 0
-            cell.bubbleView.backgroundColor = userTextColor
-            cell.bubbleViewRightAnchor?.isActive = true
-            cell.bubbleViewLeftAnchor?.isActive = false
-            if cell.bubbleView.backgroundColor == Colors.nebulaPink{
-                cell.textView.textColor = UIColor.black
-            }
-            
-        }else{
-            cell.senderHeightAnchor?.constant = 20
-            cell.bubbleView.backgroundColor = otherTextColor
-            cell.bubbleViewRightAnchor?.isActive = false
-            cell.bubbleViewLeftAnchor?.isActive = true
-            if cell.bubbleView.backgroundColor == Colors.nebulaPink{
-                cell.textView.textColor = UIColor.black
-            }
-            
-            if indexPath.row > 0 && self.currentPoolMessages[indexPath.row-1].sender != self.currentPoolMessages[indexPath.row].sender{
-                cell.senderLabel.text = self.currentPoolMessages[indexPath.row].sender
-            }
-            
-            cell.senderLeftAnchor?.isActive = true
-            cell.senderRightAnchor?.isActive = false
-        }
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "messageBubble", for: indexPath) as! MessageBubble
-        var height: CGFloat = 80
-        height = findSize(text: self.currentPoolMessages[indexPath.row].body!, label: cell.textView).height + 20
-        return CGSize(width: view.frame.width, height: height)
-    }
-    
-    func findSize(text: String, label: UITextView) -> CGRect{
-        let constraintRect = CGSize(width: 200,
-                                    height: 1000)
-        
-        return NSString(string: text).boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font as Any], context: nil)
-    }
+    var collectionMoved = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -102,7 +46,6 @@ class PoolChatVC: MessengerBaseVC, UICollectionViewDelegate, UICollectionViewDat
         topView.involvedCenterAnchor?.isActive = true
         
         //Making a collectionview
-        
         let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
         layout.sectionInset = UIEdgeInsets(top: 4, left: 0, bottom: 0, right: 0)
         layout.itemSize = CGSize(width: 375, height: 50)
@@ -172,9 +115,134 @@ class PoolChatVC: MessengerBaseVC, UICollectionViewDelegate, UICollectionViewDat
     override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(self.closeVC), name: UIApplication.willResignActiveNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.openVC), name: UIApplication.didBecomeActiveNotification, object: nil)
-        
     }
     
+    @objc func closeVC()  {
+        view.endEditing(true)
+    }
+    
+    @objc func openVC()  {
+        self.messagesCollection.reloadData()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        NotificationCenter.default.removeObserver(self)
+    }
+}
+
+// MARK: CollectionView
+extension PoolChatVC: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return currentPoolMessages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "messageBubble", for: indexPath) as! MessageBubble
+        let text = self.currentPoolMessages[indexPath.row].body
+        
+        cell.textView.text = text
+        cell.bubbleWidthAnchor?.constant = findSize(text: text!, label: cell.textView).width + 32
+        cell.senderLabel.text = ""
+        
+        cell.textView.textColor = UIColor.white
+        
+        cell.senderHeightAnchor?.constant = 0
+        
+        if self.currentPoolMessages[indexPath.row].sender == GlobalUser.username{
+            cell.senderHeightAnchor?.constant = 0
+            cell.bubbleView.backgroundColor = userTextColor
+            cell.bubbleViewRightAnchor?.isActive = true
+            cell.bubbleViewLeftAnchor?.isActive = false
+            if cell.bubbleView.backgroundColor == Colors.nebulaPink{
+                cell.textView.textColor = UIColor.black
+            }
+            
+        }else{
+            cell.senderHeightAnchor?.constant = 20
+            cell.bubbleView.backgroundColor = otherTextColor
+            cell.bubbleViewRightAnchor?.isActive = false
+            cell.bubbleViewLeftAnchor?.isActive = true
+            if cell.bubbleView.backgroundColor == Colors.nebulaPink{
+                cell.textView.textColor = UIColor.black
+            }
+            
+            if indexPath.row > 0 && self.currentPoolMessages[indexPath.row-1].sender != self.currentPoolMessages[indexPath.row].sender{
+                cell.senderLabel.text = self.currentPoolMessages[indexPath.row].sender
+            }
+            
+            cell.senderLeftAnchor?.isActive = true
+            cell.senderRightAnchor?.isActive = false
+        }
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "messageBubble", for: indexPath) as! MessageBubble
+        var height: CGFloat = 80
+        height = findSize(text: self.currentPoolMessages[indexPath.row].body!, label: cell.textView).height + 20
+        return CGSize(width: view.frame.width, height: height)
+    }
+    
+    func findSize(text: String, label: UITextView) -> CGRect{
+        let constraintRect = CGSize(width: 200, height: 1000)
+        return NSString(string: text).boundingRect(with: constraintRect, options: .usesLineFragmentOrigin, attributes: [.font: label.font as Any], context: nil)
+    }
+}
+
+
+// MARK: TextViewDelegate
+extension PoolChatVC: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        return true
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        if textView.text.count == 0{
+            modularKeyboard.sendButton.isEnabled = false
+        }else{
+            modularKeyboard.sendButton.isEnabled = true
+        }
+        UserDefaults.standard.set(textView.text, forKey: self.poolId)
+    }
+}
+
+
+// MARK: GestureRecognizers
+extension PoolChatVC: UIGestureRecognizerDelegate {
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return false
+    }
+}
+
+
+// MARK: Sockets
+extension PoolChatVC {
+    func openSocket(completion: () -> Void) {
+        SocketIOManager.socket.on("message") { ( data, ack) -> Void in
+            guard let parsedData = data[0] as? String else { return }
+            let msg = JSON.init(parseJSON: parsedData)
+            let tempMsg = TerseMessage(_id: "", //Fix this
+                sender: msg["sender"].string!,
+                body: msg["body"].string!,
+                dateTime: msg["dateTime"].string!,
+                read: false)
+            
+            guard let msgId = msg["id"].string else{
+                return
+            }
+            
+            if msgId == self.poolId{
+                self.currentPoolMessages.append(tempMsg)
+                self.messagesCollection.reloadData()
+                self.scrollToBottom(animated: true)
+            }
+        }
+    }
+}
+
+
+// MARK: Listeners
+extension PoolChatVC {
     @objc func tappedOnSubscribe(){
         if subscribed{
             PoolRoutes.removePoolSubscription(poolId: self.poolId, completion: {success in
@@ -197,33 +265,79 @@ class PoolChatVC: MessengerBaseVC, UICollectionViewDelegate, UICollectionViewDat
         }
     }
     
-    @objc func closeVC()  {
+    @objc func closeButtonPressed(){
+        if !keyboardIsUp{
+            topView.bottomBarActionButton.isHidden = false
+            modularKeyboard.closeButtonTapped(){
+                self.topView.bottomBarActionButton.alpha = 1
+                self.topView.animateLayer()
+            }
+            UIView.animate(withDuration: 0.2, animations: {
+                self.messagesCollection.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 102, right: 0)
+                self.view.layoutIfNeeded()
+            }, completion:{_ in
+                UIView.animate(withDuration: 0.2, animations: {
+                    self.messagesCollection.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 12, right: 0)
+                    self.view.layoutIfNeeded()
+                })
+            })
+        }
+    }
+    
+    @objc func draggedCircle(_ sender:UIPanGestureRecognizer){
+        switch sender.state {
+        case .began:
+            print("hi")
+        case .changed:
+            let translation = sender.translation(in: self.view)
+            if !self.keyboardIsUp{
+                modularKeyboard.draggedCircle(x: translation.x, y: translation.y)
+                sender.setTranslation(CGPoint.zero, in: self.view)
+            }
+            if !collectionMoved{
+                self.messagesCollectionBottomConstraint?.constant = 0
+                //self.collectionBottomAnchor?.constant = 100
+                let quickFrame = self.messagesCollection.frame
+                UIView.animate(withDuration: 0.3){
+                    self.messagesCollection.frame = CGRect(x: quickFrame.origin.x, y: quickFrame.origin.y, width: quickFrame.width, height: quickFrame.height+100)
+                    self.view.layoutIfNeeded()
+                }
+                print("moved")
+                self.collectionMoved = true
+            }
+        default:
+            break
+        }
+    }
+    
+    @objc func resetButton(){
+        collectionMoved = false
+        self.resetBottomBar()
+        
+        self.topView.bottomBarActionButton.isHidden = true
+        self.messagesCollectionBottomConstraint?.constant = -self.modularKeyboard.frame.height
+        UIView.animate(withDuration: 0.3, animations: {
+            self.view.layoutIfNeeded()
+        }, completion: {_ in
+            self.scrollToBottom(animated: true)
+        })
+    }
+    
+    @objc func downButtonPressed(){
         view.endEditing(true)
     }
     
-    @objc func openVC()  {
-        self.messagesCollection.reloadData()
+    @objc func sendWrapper(sender: UIButton){
+        self.sendButtonPressed(sender)
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        NotificationCenter.default.removeObserver(self)
+    @objc func goBack(sender: UIButton){
+        GlobalUser.currentConv = ""
+        self.view.endEditing(true)
+        self.dismiss(animated: true, completion: nil)
     }
     
-    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
-        
-        return true
-    }
-    
-    func textViewDidChange(_ textView: UITextView) {
-        if textView.text.count == 0{
-            modularKeyboard.sendButton.isEnabled = false
-        }else{
-            modularKeyboard.sendButton.isEnabled = true
-        }
-        UserDefaults.standard.set(textView.text, forKey: self.poolId)
-    }
-    
-    @IBAction func sendButtonPressed(_ sender: UIButton) {
+    @objc func sendButtonPressed(_ sender: UIButton) {
         let url = URL(string: sendPoolsRoute)
         
         var requestJson = [String:Any]()
@@ -273,111 +387,5 @@ class PoolChatVC: MessengerBaseVC, UICollectionViewDelegate, UICollectionViewDat
         }catch{
             print("Caught")
         }
-    }
-    
-    //MARK: Sockets
-    func openSocket(completion: () -> Void) {
-        SocketIOManager.socket.on("message") { ( data, ack) -> Void in
-            guard let parsedData = data[0] as? String else { return }
-            let msg = JSON.init(parseJSON: parsedData)
-            let tempMsg = TerseMessage(_id: "", //Fix this
-                sender: msg["sender"].string!,
-                body: msg["body"].string!,
-                dateTime: msg["dateTime"].string!,
-                read: false)
-            
-            guard let msgId = msg["id"].string else{
-                return
-            }
-            
-            if msgId == self.poolId{
-                self.currentPoolMessages.append(tempMsg)
-                self.messagesCollection.reloadData()
-                self.scrollToBottom(animated: true)
-            }
-        }
-    }
-    
-    // topView Actions
-    @objc func closeButtonPressed(){
-        if !keyboardIsUp{
-            topView.bottomBarActionButton.isHidden = false
-            modularKeyboard.closeButtonTapped(){
-                self.topView.bottomBarActionButton.alpha = 1
-                self.topView.animateLayer()
-            }
-            UIView.animate(withDuration: 0.2, animations: {
-                self.messagesCollection.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 102, right: 0)
-                self.view.layoutIfNeeded()
-            }, completion:{_ in
-                UIView.animate(withDuration: 0.2, animations: {
-                    self.messagesCollection.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 12, right: 0)
-                    self.view.layoutIfNeeded()
-                })
-            })
-        }
-    }
-    
-    var collectionMoved = false
-    @objc func draggedCircle(_ sender:UIPanGestureRecognizer){
-        switch sender.state {
-        case .began:
-            print("hi")
-        case .changed:
-            let translation = sender.translation(in: self.view)
-            if !self.keyboardIsUp{
-                modularKeyboard.draggedCircle(x: translation.x, y: translation.y)
-                sender.setTranslation(CGPoint.zero, in: self.view)
-            }
-            if !collectionMoved{
-                self.messagesCollectionBottomConstraint?.constant = 0
-                //self.collectionBottomAnchor?.constant = 100
-                let quickFrame = self.messagesCollection.frame
-                UIView.animate(withDuration: 0.3){
-                    self.messagesCollection.frame = CGRect(x: quickFrame.origin.x, y: quickFrame.origin.y, width: quickFrame.width, height: quickFrame.height+100)
-                    self.view.layoutIfNeeded()
-                }
-                print("moved")
-                self.collectionMoved = true
-            }
-        default:
-            break
-        }
-    }
-    
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return false
-    }
-    
-    @objc func resetButton(){
-        collectionMoved = false
-        self.resetBottomBar()
-        
-        self.topView.bottomBarActionButton.isHidden = true
-        self.messagesCollectionBottomConstraint?.constant = -self.modularKeyboard.frame.height
-        UIView.animate(withDuration: 0.3, animations: {
-            self.view.layoutIfNeeded()
-        }, completion: {_ in
-            self.scrollToBottom(animated: true)
-        })
-    }
-    
-    @objc func downButtonPressed(){
-        view.endEditing(true)
-    }
-    
-    @objc func sendWrapper(sender: UIButton){
-        self.sendButtonPressed(sender)
-    }
-    
-    @objc func goBack(sender: UIButton){
-        GlobalUser.currentConv = ""
-        self.view.endEditing(true)
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    //MARK: Nav
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        SocketIOManager.shutOffListener()
     }
 }
